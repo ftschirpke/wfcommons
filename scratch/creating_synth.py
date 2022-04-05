be3f1ad9dec066b5c7592973dbe509ebf6a530c0
tainagdcoleman <tainagdcoleman@Tain-WorkPC.localdomain>
2022-04-05 07:55:29 -0700

    First results for remove_base

--- scratch/creating_synth.py
+++ scratch/creating_synth.py
@@ -1,4 +1,4 @@
-from montage_recipes.montage_test import Montage_testRecipe
+from scratch.montage.montage_recipes import Montage_testRecipe
 from wfcommons.wfgen import WorkflowGenerator
 import pathlib 
 
@@ -6,11 +6,13 @@ this_dir = pathlib.Path(__file__).parent.resolve()
 recipe = Montage_testRecipe
 num_tasks = [7119, 9807]
 save_dir = this_dir.joinpath("MontageTest")
-
+save_dir.mkdir(exist_ok=True, parents=True)
 
 for num in num_tasks:
     generator = WorkflowGenerator(recipe.from_num_tasks(num))
     workflow = generator.build_workflow()
     workflow.name = f"{workflow.name.split('-')[0]}"
-    json_path = save_dir.joinpath(
-        f"{workflow.name.lower()}-{num}").with_suffix(".json")
+    json_path = save_dir.joinpath(f"{workflow.name.lower()}-{num}").with_suffix(".json")
+    
+    json_path.parent.mkdir(exist_ok=True, parents=True)
+    workflow.write_json(json_path)
